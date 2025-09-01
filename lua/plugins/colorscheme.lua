86a62377f7746b4c70e2194a7f53fc94d719900c
owen.harper-jones <owen.harper-jones@check24.de>
2025-09-01 09:06:02 +0200

    add read me

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,5 +1,16 @@
+-- Make line numbers pop with tokyonight-night
 return {
-  "folke/tokyonight.nvim",
-  lazy = true,
-  opts = { style = "night" },
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      style = "night",
+      on_highlights = function(hl, c)
+        hl.LineNr = { fg = "#c0caf5" } -- brighter gutter numbers
+        hl.CursorLineNr = { fg = c.yellow, bold = true }
+        hl.LineNrAbove = { fg = c.blue } -- relative numbers above
+        hl.LineNrBelow = { fg = c.blue } -- relative numbers below
+        hl.SignColumn = { bg = "NONE" } -- keep gutter clean
+      end,
+    },
+  },
 }
